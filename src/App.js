8056ff011fea62e191f4dfe2d1b76014a0914cd8
Jon Jaryong Lee <director9303@gmail.com>
2021-01-19 18:12:06 +0900

    210119

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-const foodILike = [
-  {
-    name: "kimchi",
-    image:
-      "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
-  },
-  {
-    name: "pork",
-    image:
-      "https://cafedelites.com/wp-content/uploads/2018/05/Honey-Garlic-Pork-Chops-IMAGE-56.jpg",
-  },
-  {
-    name: "ramen",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/1200px-Shoyu_Ramen.jpg",
-  },
-];
+import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img height="100" src={picture} alt={name}></img>
+class App extends React.Component{
+  state = {
+    counter: 0
+  };
+
+  plus = () => {
+    this.setState(current => ({ counter: current.counter + 1}));
+  };
+
+  minus = () => {
+    this.setState(current => ({ counter: current.counter - 1}));
+  };
+
+  render() {
+    return <div>
+      <h1>The number is: {this.state.counter}</h1>
+      <button onClick={ this.plus }>Plus</button>
+      <button onClick={ this.minus }>Minus</button>
     </div>
-  );
-};
-
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image}/>
-}
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(renderFood)}
-    </div>
-  );
+  }
 }
 
 export default App;
