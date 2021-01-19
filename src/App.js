acda3786f3fc372b2011da0a6ccd29dc4359917a
Jon Jaryong Lee <director9303@gmail.com>
2021-01-19 15:43:06 +0900

    210119

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+const foodILike = [
+  {
+    name: "kimchi",
+    image:
+      "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
+  },
+  {
+    name: "pork",
+    image:
+      "https://cafedelites.com/wp-content/uploads/2018/05/Honey-Garlic-Pork-Chops-IMAGE-56.jpg",
+  },
+  {
+    name: "ramen",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/1200px-Shoyu_Ramen.jpg",
+  },
+];
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img height="100" src={picture} alt={name}></img>
+    </div>
+  );
+};
+
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image}/>
+}
+
 function App() {
   return (
     <div className="App">
-      hello
+      {foodILike.map(renderFood)}
     </div>
   );
 }
