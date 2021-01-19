6519ccea9fdd3eec630d531ac40a0b5283ccfef1
Jon Jaryong Lee <director9303@gmail.com>
2021-01-20 00:56:41 +0900

    210120

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movie: []
   };
-  componentDidMount(){
+  componentDidMount() {
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 6000);
-  };
+  }
   render() {
-    const { isLoading } = this.state;  // 이것은, isLoading이 곧 this.state.isLoading임을 뜻한다.
+    // isLoading이 곧 this.state.isLoading임을 뜻한다.
+    const { isLoading } = this.state; 
 
     // 삼항연산자. true이면 앞을 실행, false이면 뒤를 실행
-    return <div>{ isLoading ? "Loading...": "We are ready" }</div>;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
