d710f5c50cdc9aa3699ffcb00565e79ab84f4a08
Jon Jaryong Lee <director9303@gmail.com>
2021-01-20 15:26:41 +0900

    211020

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: [],
   };
 
-  // async await 를 통해, axios가 끝날때까지 기다리게 한다. 
-  getMovies = async() => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-  }
-  
+  // async await 를 통해, axios가 끝날때까지 기다리게 한다.
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state; 
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
