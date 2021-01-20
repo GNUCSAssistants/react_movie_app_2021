6f7d285f9a2a88365543b142d86591a7a97192e3
Jon Jaryong Lee <director9303@gmail.com>
2021-01-20 11:53:01 +0900

    210120

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: []
   };
+
+  // async await 를 통해, axios가 끝날때까지 기다리게 한다. 
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
-    // isLoading이 곧 this.state.isLoading임을 뜻한다.
     const { isLoading } = this.state; 
-
-    // 삼항연산자. true이면 앞을 실행, false이면 뒤를 실행
     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
