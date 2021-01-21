c77fdf632becbb5ea9a8721fc6d6b9359e67cc8d
Jon Jaryong Lee <director9303@gmail.com>
2021-01-21 11:12:58 +0900

    210121

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home"
 import About from "./routes/About";
-import Navigation from "./components/Navigation"
+import MovieDetail from "./routes/MovieDetail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return <HashRouter>
     <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={MovieDetail} />
   </HashRouter>
 }
 
